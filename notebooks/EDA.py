d6e43fe5cbc4926f0fbb4e9a70896e56dd795908
Jonathan Prentice <65112458+JonathanPrentice@users.noreply.github.com>
2025-02-07 02:25:00 -0800

    Adding Kaggle database

--- notebooks/EDA.py
+++ notebooks/EDA.py
@@ -2,15 +2,24 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import kagglehub
-from kagglehub import KaggleDatasetAdapter
+import os
+import zipfile
+
+# Download the dataset from Kaggle
+os.system('kaggle datasets download -d vishakhdapat/customer-segmentation-clustering -p ./data')
+
+# Unzip the dataset
+with zipfile.ZipFile('./data/customer-segmentation-clustering.zip', 'r') as zip_ref:
+    zip_ref.extractall('./data')
+
+# Set the path to the file you'd like to load
+file_path = "./data/customer_segmentation.csv"
 
 # Load the data
-data = pd.read_csv('data.csv')
+df = pd.read_csv(file_path)
 
-# Display the first 5 rows of the data
-data.head()
+# Display the first 5 records
+print("First 5 records:", df.head())
 
-# Display the last 5 rows of the data
-data.tail()
-
+# Display the last 5 records
+print("Last 5 records:", df.tail())
